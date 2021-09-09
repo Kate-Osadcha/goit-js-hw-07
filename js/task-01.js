0eb5c9fa693f082b39e773fc75c25d808b4ee7cb
Катерина Осадчая <monzolevskaya@gmail.com>
2021-09-09 10:37:53 +0300

    task-01 добавлен еще один способ решения

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,16 @@ const categoriesEl = document.querySelectorAll(".item h2").forEach((elem) =>
 Количество элементов: ${elem.nextElementSibling.children.length}`
   )
 );
+
+//способ 2
+
+// const list = document.querySelector("#categories");
+// const items = list.querySelectorAll(".item");
+// console.log(`В списке ${items.length} категории:`);
+// items.forEach((node) => {
+//   const h2 = node.querySelector("h2");
+//   const nodeItems = node.querySelectorAll("li");
+//   console.log(
+//     ` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`
+//   );
+// });
